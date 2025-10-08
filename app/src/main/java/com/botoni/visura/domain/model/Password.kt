fea180123a45122778f6a91792f7d9c88999afbc
unknown <joaobotoni@hotmail.com>
2025-10-08 16:51:58 -0300

    impl functions

--- app/src/main/java/com/botoni/visura/domain/model/Password.kt
+++ app/src/main/java/com/botoni/visura/domain/model/Password.kt
@@ -2,7 +2,7 @@ package com.botoni.visura.domain.model
 
 import com.botoni.visura.domain.exceptions.AuthenticationException
 
-class Password(val value: String) {
+class Password(val value: String) : Comparable<Password> {
     init {
         if (value.isBlank()) {
             throw AuthenticationException("Senha é obrigatória")
@@ -11,5 +11,22 @@ class Password(val value: String) {
             throw AuthenticationException("Mínimo 8 caracteres")
         }
     }
+
+    override fun compareTo(other: Password): Int {
+        return this.value.compareTo(other.value)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Password
+        return value == other.value
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+
     override fun toString(): String = value
 }
